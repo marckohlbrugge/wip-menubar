8f4526d12a5d2fc2e3c928fd57b86102c098a581
Andres Vargas <zodman@gmail.com>
2019-01-15 17:41:24 -0600

    icons for linux

--- src/icon.js
+++ src/icon.js
@@ -19,4 +19,13 @@ switch (process.platform) {
     };
     break;
   }
+  case 'linux': {
+    module.exports = {
+      done: path.join(__dirname, 'icons/macos/doneTemplate.png'),
+      todo: path.join(__dirname, 'icons/macos/todoTemplate.png'),
+      load: path.join(__dirname, 'icons/macos/loadTemplate.png'),
+      fail: path.join(__dirname, 'icons/macos/failTemplate.png'),
+    };
+    break;
+  }
 }
