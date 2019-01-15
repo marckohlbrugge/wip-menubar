60d2e59d59fd5cbe5b47b8012e1af51f1e151718
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2019-01-16 00:27:28 +0100

    WIP: add shortcuts

--- src/app.js
+++ src/app.js
@@ -152,6 +152,28 @@ app.on('ready', () => {
   let composeWindow = null;
   let preferencesWindow = null;
 
+  // Create the Application's main menu
+  var template = [{
+    label: "Application",
+    submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+      ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
   const ret = globalShortcut.register(store.get('shortcut'), () => {
     onComposeClick();
   });
