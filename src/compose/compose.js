df5e1510c86f350d9cb5ff7c0ccde33dee606875
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2019-01-24 10:22:47 +0100

    autocomplete pending todos

--- src/compose/compose.js
+++ src/compose/compose.js
@@ -1,48 +1,13 @@
 const { ipcRenderer, remote } = require('electron');
-
+const debounce = require('lodash.debounce');
 const store = require('../store');
+const promiseIpc = require('electron-promise-ipc');
 
-const todoForm = document.getElementById('todo-form');
 const todoBody = document.getElementById('todo-body');
-const todoIcon = document.querySelector('.icon i.fas');
 
-todoBody.focus();
-
-todoBody.addEventListener('input', () => {
-  if (todoBody.value.match(/^\/todo\b/i)) {
-    todoIcon.classList.remove('fa-check');
-    todoIcon.classList.add('fa-hourglass-half');
-  } else {
-    todoIcon.classList.remove('fa-hourglass-half');
-    todoIcon.classList.add('fa-check');
-  }
-});
-
-todoForm.addEventListener('submit', event => {
-  event.preventDefault();
-  todoBody.disabled = true;
-  todoIcon.classList.remove('fa-check', 'fa-hourglass-half');
-  todoIcon.classList.add('fa-spinner', 'fa-spin');
-  // if (isInvalid(todoBody)) return;
-  ipcRenderer.send('createTodo', todoBody.value);
-});
-
-ipcRenderer.on('todoCreated', (event, todo) => {
-  todoIcon.classList.remove('fa-spinner', 'fa-spin');
-
-  if (todo.completed_at) {
-    todoIcon.classList.add('fa-check', 'success');
-  } else {
-    todoIcon.classList.add('fa-hourglass-half', 'success');
-  }
-
+ipcRenderer.on('todoSaved', (event, todo) => {
   var window = remote.getCurrentWindow();
   window.close();
-
-  // wipUsername.parentElement.classList.remove('is-loading');
-  // wipUsername.classList.toggle('is-danger', !userExists);
-  // wipUsernameStatus.classList.toggle('fa-check', userExists);
-  // wipUsernameStatus.classList.toggle('fa-times', !userExists);
 });
 
 function isInvalid(input) {
@@ -50,3 +15,77 @@ function isInvalid(input) {
   input.classList.toggle('is-warning', valid);
   return valid;
 }
+
+const example = {
+  data() {
+    return {
+      data: [],
+      name: '',
+      selected: null,
+      isFetching: false,
+      icon: 'check',
+    };
+  },
+  methods: {
+    keydown: function() {
+      if (this.name.match(/^\/todo\b/i)) {
+        this.icon = 'hourglass-half';
+      } else {
+        this.icon = 'check';
+        this.getAsyncData();
+      }
+    },
+    submitForm: function() {
+      todoBody.disabled = true;
+
+      if (app.selected) {
+        // Completed an existing todo
+        ipcRenderer.send('completeTodo', app.selected.id);
+        console.log('selected', app.selected.id);
+      } else {
+        // New todo
+        ipcRenderer.send('createTodo', app.name);
+        console.log(app.name);
+        console.log('new todo');
+      }
+
+      // if (isInvalid(todoBody)) return;
+    },
+    getAsyncData: debounce(function() {
+      if (!this.name.length) {
+        this.data = [];
+        return;
+      }
+      this.isFetching = true;
+
+      promiseIpc
+        .send('fetchPendingTodos', this.name)
+        .then(todos => {
+          this.data = todos;
+        })
+        .catch(error => {
+          this.data = [];
+          throw error;
+        })
+        .finally(() => {
+          this.isFetching = false;
+        });
+    }, 500),
+  },
+};
+
+const app = new Vue(example);
+app.$mount('#app');
+
+const form = new Vue({
+  // our data
+  data: {
+    name: '',
+    email: '',
+  },
+
+  // our methods
+  methods: {},
+});
+
+form.$mount('#todo-form');
