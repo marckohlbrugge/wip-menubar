e91b8d940f78dd09d5bc8da3a266ba5df634bef7
Praneet Rohida <praneetrohida@gmail.com>
2022-05-22 21:52:38 +0530

    add an after-sign hook for notarization

---
+++ scripts/after-sign-hook.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const { notarize } = require('electron-notarize');
+const pRetry = require('p-retry');
+
+module.exports = async function (params) {
+  const { electronPlatformName, appOutDir } = params;
+  const { SKIP_SIGN } = process.env;
+
+  // Only notarize the app on Mac OS only.
+  if (electronPlatformName !== 'darwin' || SKIP_SIGN === 'true') {
+    return;
+  }
+
+  console.log('afterSign hook triggered');
+
+  // Same appId in electron-builder.
+  let appId = 'chat.wip.menubar';
+
+  let appPath = path.join(
+    appOutDir,
+    `${params.packager.appInfo.productFilename}.app`,
+  );
+
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
+
+  console.log(`Notarizing ${appId} found at ${appPath}`);
+
+  const notarizeConfig = {
+    appBundleId: appId,
+    appPath: appPath,
+    appleApiKey: `~/private_keys/AuthKey_${process.env.API_KEY_ID}.p8`,
+    appleApiKeyId: process.env.API_KEY_ID,
+    appleApiIssuer: process.env.APPLE_ISSUER_ID,
+  };
+
+  await pRetry(
+    (attempt) => {
+      console.log('Notarization attempt: ', attempt);
+      return notarize(notarizeConfig);
+    },
+    {
+      retries: 5,
+      onFailedAttempt: (error) => {
+        console.log(
+          `Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+        );
+      },
+    },
+  );
+
+  console.log(`Done notarizing ${appId}`);
+};
